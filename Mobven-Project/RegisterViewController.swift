1236d448087ee8679a50fd084ff1830a6e1b161f
Zehra Karaman <zehrakaramaan@gmail.com>
2023-03-05 18:59:40 +0300

    Fix the register view

--- Mobven-Project/RegisterViewController.swift
+++ Mobven-Project/RegisterViewController.swift
@@ -9,21 +9,22 @@ import UIKit
 
 class RegisterViewController: UIViewController {
 
+    @IBOutlet weak var accountImageButton: UIButton!
+    @IBOutlet weak var nameTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        nameTextField.configure()
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        view.addGestureRecognizer(tap)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
     }
-    */
-
+    
+    @IBAction func imageButtonTapped(_ sender: UIButton) {
+    }
 }
