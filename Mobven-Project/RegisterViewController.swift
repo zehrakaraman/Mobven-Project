e1251fb940827ec0076ce1173d49606055750588
Zehra Karaman <zehrakaramaan@gmail.com>
2023-03-05 20:54:24 +0300

    Fix the keyboard view

--- Mobven-Project/RegisterViewController.swift
+++ Mobven-Project/RegisterViewController.swift
@@ -12,13 +12,48 @@ class RegisterViewController: UIViewController {
     @IBOutlet weak var accountImageButton: UIButton!
     @IBOutlet weak var nameTextField: UITextField!
     
+    var isShowingKeybord = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         nameTextField.configure()
         
-        let tap = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        // call the 'keyboardWillShow' function when the view controller receive the notification that a keyboard is going to be shown
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        
+        // call the 'keyboardWillHide' function when the view controlelr receive notification that keyboard is going to be hidden
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(keyboardWillHide))
         view.addGestureRecognizer(tap)
+    }
+    
+    @objc func keyboardWillShow(notification: NSNotification) {
+        guard let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else {
+            // if keyboard size is not available for some reason, ddont do anything
+            return
+        }
+                
+        var shouldMoveViewUp = false
+                
+        let bottomOfTextField = nameTextField.convert(nameTextField.bounds, to: self.view).maxY
+        let topOfKeyboard = self.view.frame.height - keyboardSize.height
+                
+        if bottomOfTextField > topOfKeyboard {
+            shouldMoveViewUp = true
+        }
+                
+        if shouldMoveViewUp {
+            // move to root view up by the distance of keyboard height
+            self.view.frame.origin.y = 0 - keyboardSize.height
+        }
+    }
+    
+    @objc func keyboardWillHide(notification: NSNotification) {
+        // move back to root view origin to zero
+        dismissKeyboard()
+        self.view.frame.origin.y = 0
     }
     
     @objc func dismissKeyboard() {
@@ -27,4 +62,8 @@ class RegisterViewController: UIViewController {
     
     @IBAction func imageButtonTapped(_ sender: UIButton) {
     }
+    
+    @IBAction func nextButtonTapped(_ sender: UIButton) {
+    }
 }
+
