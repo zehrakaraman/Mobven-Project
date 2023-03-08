3d3879778595b986a6f1f21a36a05554e76a6542
Zehra Karaman <zehrakaramaan@gmail.com>
2023-03-09 01:07:18 +0300

    Fix the text field view when the keyboard appears

--- Mobven-Project/Views/RegisterViewController.swift
+++ Mobven-Project/Views/RegisterViewController.swift
@@ -11,8 +11,9 @@ class RegisterViewController: UIViewController {
 
     @IBOutlet weak var accountImageButton: UIButton!
     @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var topConstraint: NSLayoutConstraint!
     
-    var isShowingKeybord = false
+    var constraint: CGFloat = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,37 +22,34 @@ class RegisterViewController: UIViewController {
         
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
         
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillHideNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
         
-        let tap = UITapGestureRecognizer(target: self, action: #selector(keyboardWillHide))
+        let tap = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
         view.addGestureRecognizer(tap)
     }
     
     @objc func keyboardWillShow(notification: NSNotification) {
-        guard let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else {
-            // if keyboard size is not available for some reason, ddont do anything
-            return
-        }
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            if keyboardSize.origin.y < nameTextField.frame.origin.y {
+                constraint += nameTextField.frame.origin.y - keyboardSize.origin.y
+                UIView.animate(withDuration: 0.1, animations: { () -> Void in
+                    self.topConstraint.constant -= (self.constraint + self.nameTextField.frame.height + 5)
+                    self.view.layoutIfNeeded()
+                })
                 
-        var shouldMoveViewUp = false
-                
-        let bottomOfTextField = nameTextField.convert(nameTextField.bounds, to: self.view).maxY
-        let topOfKeyboard = self.view.frame.height - keyboardSize.height
-                
-        if bottomOfTextField > topOfKeyboard {
-            shouldMoveViewUp = true
-        }
-                
-        if shouldMoveViewUp {
-            // move to root view up by the distance of keyboard height
-            self.view.frame.origin.y = 0 - keyboardSize.height
+            }
         }
     }
     
     @objc func keyboardWillHide(notification: NSNotification) {
-        // move back to root view origin to zero
-        dismissKeyboard()
-        self.view.frame.origin.y = 0
+        if constraint > 0 {
+            UIView.animate(withDuration: 0.2, animations: { () -> Void in
+                self.topConstraint.constant += (self.constraint + self.nameTextField.frame.height + 5)
+                self.constraint = 0
+                self.view.layoutIfNeeded()
+                self.dismissKeyboard()
+            })
+        }
     }
     
     @objc func dismissKeyboard() {
